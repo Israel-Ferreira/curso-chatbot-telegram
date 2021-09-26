6a1a5caafc5e516d24af4b139ea1158f2f950400
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-09-26 19:28:07 -0300

    Primeiro bot telegram

--- my-first-bot/app.js
+++ my-first-bot/app.js
@@ -1,2 +1,31 @@
-const Telegraph = require('telegraf')
+const Telegraf = require('telegraf').Telegraf
+const env = require('dotenv')
 
+env.config()
+
+const token = process.env.BOT_TOKEN
+
+const bot = new Telegraf(token)
+
+bot.start(ctx => {
+    const fromName = ctx.update.message.from
+    console.log(fromName)
+
+    ctx.reply(`Seja bem vindo, ${fromName.first_name}`)
+})
+
+
+bot.on('text', async (ctx, next) => {
+    await ctx.reply("Mid1")
+    next()
+})
+
+bot.on('text', async (ctx) => {
+    await ctx.reply("Mid2")
+})
+
+bot.on('sticker', async (ctx) => {
+    await ctx.reply("Sticker Received")
+})
+
+bot.startPolling()
