7d1fd61553645f0f4c160f61b2b6d2599b966a6a
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-09-26 20:37:19 -0300

    Challenge Done

---
+++ desafios/unicoUsuario.js
@@ -0,0 +1,28 @@
+const Telegraf = require('telegraf').Telegraf
+
+require('dotenv').config()
+
+const token = process.env.BOT_TOKEN
+const meisterId = process.env.USER_ID
+
+
+const bot = new Telegraf(token)
+
+bot.start((ctx, next) => {
+    const {id} = ctx.from
+    console.log(ctx.from)
+
+    if(id == meisterId){
+        ctx.reply(`Ao seu dispor, meu Championship!!`)
+        ctx.reply(`Hello, ${ctx.from.first_name}`)
+    }else{
+        ctx.reply("Desculpe, só posso falar com o meu mestre!!")
+    }
+   
+})
+
+
+
+bot.start
+
+bot.startPolling()
